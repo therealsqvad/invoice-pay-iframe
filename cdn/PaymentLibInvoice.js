4bec59a05a0324d7d8005704538d5338de50285c
Nikita Klepov <nk@invoice.su>
2020-04-24 12:16:08 +0700

    Add new page

--- cdn/PaymentLibInvoice.js
+++ cdn/PaymentLibInvoice.js
@@ -1,19 +1,10 @@
 var Invoice = {
-InvoiceRenderPaymentPage: (orderId) => {
-    const InvoiceContainer = document.getElementById('InvoicePayContainer');
-    const InvoicePaymentFrame = document.createElement('iframe');
-    InvoicePaymentFrame.id = 'InvoicePaymentFrame';
-    InvoicePaymentFrame.setAttribute('src', `https://paytest.invoice.su/frame/p${orderId}`);
-    InvoiceContainer.innerHTML =InvoicePaymentFrame.outerHTML;
+    InvoiceRenderModalFrame: (orderId) => {
+        document.getElementById('InvoiceContainer').innerHTML = `<div class="modal fade bd-example-modal-lg" id="payInvoiceModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" id="invoice-modal-dialog" role="document">class="modal-content" id="invoice-modal-content"><div class="modal-body" id="invoice-modal-body"><div id="InvoicePayContainer"/></div></div></div></div>`
+        const InvoiceContainer = document.getElementById('InvoicePayContainer');
+        const InvoicePaymentFrame = document.createElement('iframe');
+        InvoicePaymentFrame.id = 'InvoicePaymentFrame';
+        InvoicePaymentFrame.setAttribute('src', `https://paytest.invoice.su/frame/p${orderId}`);
+        InvoiceContainer.innerHTML = InvoicePaymentFrame.outerHTML;
+    }
 }
-}
-
-document.getElementById('InvoiceContainer').innerHTML = `<div class="modal fade bd-example-modal-lg" id="payInvoiceModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" id="invoice-modal-dialog" role="document">
-        <div class="modal-content" id="invoice-modal-content">
-        <div class="modal-body" id="invoice-modal-body">
-        <div id="InvoicePayContainer"/>
-        </div>
-        </div>
-        </div>
-        </div>`
