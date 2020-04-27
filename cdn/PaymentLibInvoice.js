8cd64f91cb32ae1df3f83a7717462fd8ab14f28d
Dmitry <zeppelin95@yandex.ru>
2020-04-27 12:13:22 +0700

    Modal fix

--- cdn/PaymentLibInvoice.js
+++ cdn/PaymentLibInvoice.js
@@ -1,6 +1,6 @@
 var Invoice = {
     InvoiceRenderModalFrame: (orderId) => {
-        document.getElementById('InvoiceContainer').innerHTML = `<div class="modal fade bd-example-modal-lg" id="payInvoiceModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" id="invoice-modal-dialog" role="document"><div class="modal-content" id="invoice-modal-content"><div class="modal-body" id="invoice-modal-body"><div id="InvoicePayContainer"/></div></div></div></div>`
+        document.getElementById('InvoiceContainer').innerHTML = `<div class="modal fade bd-example-modal-lg p-0" id="payInvoiceModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" id="invoice-modal-dialog" role="document"><div class="modal-content" id="invoice-modal-content"><div class="modal-body" id="invoice-modal-body"><div id="InvoicePayContainer"/></div></div></div></div>`
         const InvoiceContainer = document.getElementById('InvoicePayContainer');
         const InvoicePaymentFrame = document.createElement('iframe');
         InvoicePaymentFrame.id = 'InvoicePaymentFrame';
